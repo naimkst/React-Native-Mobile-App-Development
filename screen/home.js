3fc55ea7a576faa9b423215b23c3ae84d50ba64a
Naim Hossain <najmulspark@gmail.com>
2022-06-22 20:04:09 +0600

    Feature Product Slider part done

--- screen/home.js
+++ screen/home.js
@@ -437,57 +437,187 @@ export default function Home() {
           </View>
         </View>
 
-        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-          <View
-            style={{
-              backgroundColor: '#fff',
-              borderRadius: 10,
-              paddingHorizontal: 12,
-              paddingVertical: 20,
-              width: 156,
-            }}>
-            <View>
-              <Image source={require('../assets/images/headphone.png')} />
-            </View>
-            <View>
-              <Text
-                style={{
-                  fontSize: 14,
-                  fontWeight: '600',
-                  color: '#0C1A30',
-                  marginTop: 20,
-                }}>
-                TMA-2 HD Wireless
-              </Text>
-              <Text
-                style={{
-                  fontSize: 12,
-                  fontWeight: '700',
-                  color: '#FE3A30',
-                  marginTop: 7,
-                }}>
-                Rp. 1.500.000
-              </Text>
-            </View>
-
-            <View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            marginTop: 30,
+          }}>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+            <View
+              style={{
+                backgroundColor: '#fff',
+                borderRadius: 10,
+                paddingHorizontal: 12,
+                paddingVertical: 20,
+                width: 156,
+                marginRight: 15,
+              }}>
               <View>
-                <View>
-                  <Image source={require('../assets/images/star.png')} />
+                <Image source={require('../assets/images/headphone.png')} />
+              </View>
+              <View>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    fontWeight: '600',
+                    color: '#0C1A30',
+                    marginTop: 20,
+                  }}>
+                  TMA-2 HD Wireless
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 12,
+                    fontWeight: '700',
+                    color: '#FE3A30',
+                    marginTop: 7,
+                  }}>
+                  Rp. 1.500.000
+                </Text>
+              </View>
+
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  marginTop: 15,
+                }}>
+                <View style={{flexDirection: 'row'}}>
+                  <View style={{flexDirection: 'row', marginRight: 10}}>
+                    <Image
+                      style={{marginRight: 7}}
+                      source={require('../assets/images/star.png')}
+                    />
+                    <Text style={{fontSize: 10}}>4.6</Text>
+                  </View>
+                  <View>
+                    <Text style={{fontSize: 10}}>86 Reviews </Text>
+                  </View>
                 </View>
                 <View>
-                  <Text>86 Reviews</Text>
+                  <Image source={require('../assets/images/vector.png')} />
                 </View>
               </View>
-              <View></View>
             </View>
-          </View>
 
-          <View style={{backgroundColor: '#fff', borderRadius: 10}}>
-            <View>
-              <Image source={require('../assets/images/headphone.png')} />
+            <View
+              style={{
+                backgroundColor: '#fff',
+                borderRadius: 10,
+                paddingHorizontal: 12,
+                paddingVertical: 20,
+                width: 156,
+                marginRight: 15,
+              }}>
+              <View>
+                <Image source={require('../assets/images/product02.png')} />
+              </View>
+              <View>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    fontWeight: '600',
+                    color: '#0C1A30',
+                    marginTop: 20,
+                  }}>
+                  TMA-2 HD Wireless
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 12,
+                    fontWeight: '700',
+                    color: '#FE3A30',
+                    marginTop: 7,
+                  }}>
+                  Rp. 1.500.000
+                </Text>
+              </View>
+
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  marginTop: 15,
+                }}>
+                <View style={{flexDirection: 'row'}}>
+                  <View style={{flexDirection: 'row', marginRight: 10}}>
+                    <Image
+                      style={{marginRight: 7}}
+                      source={require('../assets/images/star.png')}
+                    />
+                    <Text style={{fontSize: 10}}>4.6</Text>
+                  </View>
+                  <View>
+                    <Text style={{fontSize: 10}}>86 Reviews </Text>
+                  </View>
+                </View>
+                <View>
+                  <Image source={require('../assets/images/vector.png')} />
+                </View>
+              </View>
             </View>
-          </View>
+
+            <View
+              style={{
+                backgroundColor: '#fff',
+                borderRadius: 10,
+                paddingHorizontal: 12,
+                paddingVertical: 20,
+                width: 156,
+                marginRight: 15,
+              }}>
+              <View>
+                <Image source={require('../assets/images/product03.png')} />
+              </View>
+              <View>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    fontWeight: '600',
+                    color: '#0C1A30',
+                    marginTop: 20,
+                  }}>
+                  TMA-2 HD Wireless
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 12,
+                    fontWeight: '700',
+                    color: '#FE3A30',
+                    marginTop: 7,
+                  }}>
+                  Rp. 1.500.000
+                </Text>
+              </View>
+
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  marginTop: 15,
+                }}>
+                <View style={{flexDirection: 'row'}}>
+                  <View style={{flexDirection: 'row', marginRight: 10}}>
+                    <Image
+                      style={{marginRight: 7}}
+                      source={require('../assets/images/star.png')}
+                    />
+                    <Text style={{fontSize: 10}}>4.6</Text>
+                  </View>
+                  <View>
+                    <Text style={{fontSize: 10}}>86 Reviews </Text>
+                  </View>
+                </View>
+                <View>
+                  <Image source={require('../assets/images/vector.png')} />
+                </View>
+              </View>
+            </View>
+          </ScrollView>
         </View>
       </View>
     </>
